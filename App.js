a8142aad32b33259c3e641f985fcfed02c18dd4a
AndreGuilhermeCorrea <de_correa@msn.com>
2024-03-04 08:41:54 -0300

    Create tela login

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Bem vindo!</Text>
+      <Text>Sistema</Text>
+      <Text style={styles.title}>Comanda</Text>
+      <TextInput style={styles.input} placeholder="email" />
+      <TextInput style={styles.input} placeholder='senha' secureTextEntry /> 
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FCFBFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '121A2C',
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  input:{
+    padding:8,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 20,
+    borderRadius: 4
+  }
+
 });
